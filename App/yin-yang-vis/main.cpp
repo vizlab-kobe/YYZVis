0777b2716ee1f65110ee292ee99221a2a1d9b67f
tsubota-cs52 <1215053t@stu.kobe-u.ac.jp>
2016-03-03 16:49:53 +0900

    Modified.

--- App/yin-yang-vis/main.cpp
+++ App/yin-yang-vis/main.cpp
@@ -1,10 +1,10 @@
-#include "main_read_kvsml.h"
+//#include "main_read_kvsml.h"
 //#include "main_write_kvsml.h"
-//#include "main_yin_yang_vis.h"
+#include "main_yin_yang_vis.h"
 
 int main( int argc, char** argv )
 {
-  return main_read_kvsml( argc, argv );
+  //return main_read_kvsml( argc, argv );
   //return main_write_kvsml( argc, argv );
-  //return main_yin_yang_vis( argc, argv );
+  return main_yin_yang_vis( argc, argv );
 }
