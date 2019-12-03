67a900e2fc1daf37ed9fa6deec09b566a489db85
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2019-12-03 12:33:54 +0900

    Update ExternalFaces.cpp

--- App/ExternalFace/ExternalFaces.cpp
+++ App/ExternalFace/ExternalFaces.cpp
@@ -45,6 +45,10 @@ ExternalFaces::SuperClass* ExternalFaces::exec( const kvs::ObjectBase* object )
     BaseClass::setRange( volume );
     BaseClass::setMinMaxCoords( volume, this );
 
+    SuperClass::setPolygonType( kvs::PolygonObject::Triangle );
+    SuperClass::setColorType( kvs::PolygonObject::VertexColor );
+    SuperClass::setNormalType( kvs::PolygonObject::PolygonNormal );
+
     if ( YinYangVis::ZhongVolumeObject::DownCast( volume ) )
     {
         const YinYangVis::ZhongVolumeObject* zvolume = YinYangVis::ZhongVolumeObject::DownCast( volume );
@@ -56,48 +60,45 @@ ExternalFaces::SuperClass* ExternalFaces::exec( const kvs::ObjectBase* object )
         this->mapping( yvolume );
     }
 
-    SuperClass::setPolygonType( kvs::PolygonObject::Triangle );
-    SuperClass::setColorType( kvs::PolygonObject::VertexColor );
-    SuperClass::setNormalType( kvs::PolygonObject::PolygonNormal );
-    if ( SuperClass::numberOfOpacities() == 0 ) SuperClass::setOpacity( 255 );
-
     return this;
 }
 
 void ExternalFaces::mapping( const YinYangVis::ZhongVolumeObject* zvolume )
 {
-    //const size_t nfaces = NUMBER_OF_FACES;
-    //const size_t nverts = NUMBER_OF_VERTICES;
-    //const kvs::ColorMap cmap = BaseClass::colorMap();
+    // const size_t nfaces = NUMBER_OF_FACES;
+    // const size_t nverts = NUMBER_OF_VERTICES;
+    // const kvs::ColorMap cmap = BaseClass::colorMap();
 
-    //kvs::ValueArray<kvs::Real32> coords( nverts * 3 );
-    //kvs::ValueArray<kvs::UInt8> colors( nverts * 3 );
-    //kvs::ValueArray<kvs::Real32> normals( nfaces * 3 );
+    // kvs::ValueArray<kvs::Real32> coords( nverts * 3 );
+    // kvs::ValueArray<kvs::UInt8> colors( nverts * 3 );
+    // kvs::ValueArray<kvs::Real32> normals( nfaces * 3 );
 
     // Calculate coords, colors, and normals.
     // ...
 
-    //SuperClass::setCoords( coords );
-    //SuperClass::setColors( colors );
-    //SuperClass::setNormals( normals );
+    // SuperClass::setCoords( coords );
+    // SuperClass::setColors( colors );
+    // SuperClass::setNormals( normals );
+    // if ( SuperClass::numberOfOpacities() == 0 ) SuperClass::setOpacity( 255 );
 }
 
 void ExternalFaces::mapping( const YinYangVis::YinYangVolumeObject* yvolume )
 {
-    //const size_t nfaces = NUMBER_OF_FACES;
-    //const size_t nverts = NUMBER_OF_VERTICES;
-    //const kvs::ColorMap cmap = BaseClass::colorMap();
+    // const size_t nfaces = NUMBER_OF_FACES;
+    // const size_t nverts = NUMBER_OF_VERTICES;
+    // const kvs::ColorMap cmap = BaseClass::colorMap();
 
-    //kvs::ValueArray<kvs::Real32> coords( nverts * 3 );
-    //kvs::ValueArray<kvs::UInt8> colors( nverts * 3 );
-    //kvs::ValueArray<kvs::Real32> normals( nfaces * 3 );
+    // kvs::ValueArray<kvs::Real32> coords( nverts * 3 );
+    // kvs::ValueArray<kvs::UInt8> colors( nverts * 3 );
+    // kvs::ValueArray<kvs::Real32> normals( nfaces * 3 );
 
     // Calculate coords, colors, and normals.
     // ...
 
-    //SuperClass::setCoords( coords );
-    //SuperClass::setColors( colors );
-    //SuperClass::setNormals( normals );
+    // SuperClass::setCoords( coords );
+    // SuperClass::setColors( colors );
+    // SuperClass::setNormals( normals );
+    // if ( SuperClass::numberOfOpacities() == 0 ) SuperClass::setOpacity( 255 );
 }
 
 } // end of namespace YinYangVis
